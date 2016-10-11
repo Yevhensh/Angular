4a019053bfd2a62c42d12902fc8a3e46183b6c50
Genya <Yevhen.Shabala@gmail.com>
2016-10-11 23:13:58 +0300

    Adding bower

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,10 @@
 var angular = require('angular'),
     mainModule = require('./main/main.module.js');
+require('@angular/router/angular1/angular_1_router');
 
 angular.module('app',
     [
-      mainModule.name
+      mainModule.name,
     ])
   .config(function($locationProvider) {
     $locationProvider.html5Mode(true);
