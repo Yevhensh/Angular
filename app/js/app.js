ca8740457c609b7f9866f71bc3205dafb19eecda
Genya <Yevhen.Shabala@gmail.com>
2016-10-12 15:02:41 +0300

    Resolving

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,10 @@
 var angular = require('angular'),
     mainModule = require('./main/main.module.js');
-require('@angular/router/angular1/angular_1_router');
 
 angular.module('app',
     [
       mainModule.name,
+      require('angular-ui-router')
     ])
   .config(function($locationProvider) {
     $locationProvider.html5Mode(true);
