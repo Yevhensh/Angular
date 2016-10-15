23dae12ff6f5ae2043c2f7b77a89de6ad3a29d68
grek-andrian <gretsko.andrian@gmail.com>
2016-10-15 11:00:05 +0300

    Add links and about page

--- app/js/login/login.module.js
+++ app/js/login/login.module.js
@@ -1,4 +1,4 @@
-var angular = require('angular')
+var angular = require('angular'),
     loginComponent   = require('./components/login.component.js'),
     loginRoute       = require('./login.route.js');
   module.exports = angular.module("app.login", [
