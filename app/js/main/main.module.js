cfcdd9a66b87e2cac813e0eed0ec83ce4e87ba0d
Genya <Yevhen.Shabala@gmail.com>
2016-10-18 21:38:32 +0300

    Refactor index

--- app/js/main/main.module.js
+++ app/js/main/main.module.js
@@ -4,12 +4,14 @@ var carsComponent = require('../cars/car.module.js'),
     schoolsComponent = require('../schools/schools.module.js'),
     albumsModule = require('../albums/albums.module.js'),
     mainRoute = require('./main.route'),
+    indexComponent = require('../index/index.module.js'),
     mainComponent = require('./components/main.component.js');
 
 module.exports = angular.module("app.main", [
   // modules
   require('angular-ui-router'),
   require('angular-resource'),
+  indexComponent.name,
   carsComponent.name,
   mainComponent.name,
   studentsComponent.name,
