4133acd7e1e5d5a4f43f61c1446613a1757b3e1f
lahotskyy <taras.lahotskyy@gmail.com>
2016-10-19 16:39:54 +0300

    work all

--- app/js/students/students.module.js
+++ app/js/students/students.module.js
@@ -3,7 +3,7 @@ var studentListModule = require('../students/student-list/student-list.module.js
     studentCreateModule = require('../students/student-create/student-create.module.js'),
     studentEditModule = require('../students/student-edit/student-edit.module.js');
 
-module.exports = angular.module("app.students", [
+module.exports = angular.module('app.students', [
   studentCreateModule.name,
   studentEditModule.name,
   studentListModule.name,
