e78d383ab15503b33c805c292b6dfe981912d32d
Genya <Yevhen.Shabala@gmail.com>
2016-10-20 09:16:14 +0300

    Adding levels to main module

--- app/js/main/main.module.js
+++ app/js/main/main.module.js
@@ -1,22 +1,22 @@
-var carsComponent = require('../cars/car.module.js'),
-    contactsComponent = require('../contacts/contacts.module.js'),
+var contactsComponent = require('../contacts/contacts.module.js'),
     studentsComponent = require('../students/students.module.js'),
     schoolsComponent = require('../schools/schools.module.js'),
     albumsModule = require('../albums/albums.module.js'),
     mainRoute = require('./main.route'),
     indexComponent = require('../index/index.module.js'),
-    mainComponent = require('./components/main.component.js');
+    mainComponent = require('./components/main.component.js'),
+    levelsComponent = require('../levels/levels.module.js');
 
 module.exports = angular.module("app.main", [
   // modules
   require('angular-ui-router'),
   require('angular-resource'),
-  carsComponent.name,
   mainComponent.name,
   indexComponent.name,
   studentsComponent.name,
   albumsModule.name,
   schoolsComponent.name,
   contactsComponent.name,
+  levelsComponent.name,
   mainRoute.name
 ]);
