7fe99af4fb0dec4e23a0a32628fbcd8df086f756
Genya <Yevhen.Shabala@gmail.com>
2016-10-24 16:18:23 +0300

    Revert "Merge pull request #28 from Yevhensh/LWR-208"
    This reverts commit 5d26a6df6b60c1b6d39c9b53de34bbc6bab5870f, reversing
    changes made to ac7794f060d3dbd7e6f0e5e6e4a07750a1494daf.

--- app/js/students/students.module.js
+++ app/js/students/students.module.js
@@ -1,15 +1,10 @@
-var studentService = require('./student.service.js'),
-    studentListComponent = require('./list/student-list.component.js'),
-    studentDetailComponent = require('./detail/student-detail.component.js'),
-    studentCreateComponent = require('./create/student-create.component.js'),
-    studentEditComponent = require('./edit/student-edit.component.js'),
-    studentsRoute = require('./students.route.js');
+var angular = require('angular')
+    studentsComponent = require('./components/students.component.js'),
+    studentsRoute = require('./students.route.js'),
+    studentsResource = require('./services/students.service.js');
 
 module.exports = angular.module('app.students', [
-  studentService.name,
-  studentCreateComponent.name,
-  studentEditComponent.name,
-  studentListComponent.name,
-  studentDetailComponent.name,
-  studentsRoute.name
+  studentsComponent.name,
+  studentsRoute.name,
+  studentsResource.name
 ]);
--- app/js/students/students.route.js
+++ app/js/students/students.route.js
@@ -1,24 +1,10 @@
 'use strict';
-
 module.exports = angular
   .module('app.students.route', [])
   .config(function($stateProvider) {
-
     $stateProvider.state({
-      name: 'students',
-      url: '/students',
-      template: '<student-list></student-list>'
-    }).state({
-      name: 'studentEdit',
-      url: '/students/{studentId}/edit',
-      template: '<student-edit></student-edit>'
-    }).state({
-      name: 'studentDetail',
-      url: 'students/{studentId}',
-      template: '<student-detail></student-detail>'
-    }).state({
-      name: 'studentCreate',
-      url: '/students/new',
-      template: '<student-create></student-create>'
-    });
+  	  name: 'students',
+  	  url: '/students',
+  	  template: '<students></students>'
+	  });
   });
