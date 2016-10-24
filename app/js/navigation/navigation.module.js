b787e29611e7836d02f22a162b48262a5f5a631f
Genya <Yevhen.Shabala@gmail.com>
2016-10-24 21:30:50 +0300

    Delete index component add navigation component

---
+++ app/js/navigation/navigation.module.js
@@ -0,0 +1,6 @@
+var navigationComponent = require('./components/navigation.component.js');
+
+module.exports = angular.module("app.navigation", [
+    //components
+    navigationComponent.name
+]);
