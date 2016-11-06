35313f1f69cb89425fa36635d333a52fa204aa6a
zoryana94 <zoryanavakh@gmail.com>
2016-11-07 01:08:46 +0200

    Modify the albums

--- app/js/albums/albums.module.js
+++ app/js/albums/albums.module.js
@@ -1,7 +1,7 @@
-var albumListComponent = require('./album-list/album-list.component.js'),
-    albumDetailComponent = require('./album-detail/album-detail.component.js'),
-    albumCreateComponent = require('./album-create/album-create.component.js'),
-    albumEditComponent = require('./album-edit/album-edit.component.js'),
+var albumListComponent = require('./components/album-list/album-list.component.js'),
+    albumDetailComponent = require('./components/album-detail/album-detail.component.js'),
+    albumCreateComponent = require('./components/album-create/album-create.component.js'),
+    albumEditComponent = require('./components/album-edit/album-edit.component.js'),
     albumService = require('./album.service.js'),
     albumsRoute = require('./albums.route.js');
 
--- app/js/albums/albums.route.js
+++ app/js/albums/albums.route.js
@@ -3,30 +3,21 @@
 module.exports = angular
   .module('app.albums.route', [])
   .config(function($stateProvider) {
-    
-    var albumList = {
-  	  name: 'albums',
-  	  url: '/albums',
-  	  template: '<album-list></album-list>'
-    };
-    var albumDetail = {
-      name: 'album',
-      url: 'albums/{albumId}',
-      template: '<album-detail></album-detail>'
-    };
-    var albumCreate = {
-      name: 'albumCreate',
-      url: '/albums/new',
-      template: '<album-create></album-create>'
-    };
-    var albumEdit = {
-      name: 'albumEdit',
-      url: '/albums/{albumId}/edit',
-      template: '<album-edit></album-edit>'
-    };
-
-    $stateProvider.state(albumList);
-    $stateProvider.state(albumDetail);
-    $stateProvider.state(albumCreate);
-    $stateProvider.state(albumEdit);
+    $stateProvider.state({
+        name: 'albums',
+        url: '/albums',
+        template: '<album-list></album-list>'
+    }).state({
+        name: 'albumCreate',
+        url: '/albums/new',
+        template: '<album-create></album-create>'
+    }).state({
+        name: 'albumEdit',
+        url: '/albums/{albumId}/edit',
+        template: '<album-edit></album-edit>'
+    }).state({
+        name: 'album',
+        url: '/albums/{albumId}',
+        template: '<album-detail></album-detail>'
+    });
 });
