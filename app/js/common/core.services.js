860a4dec27a3aa0ac0db8c83cbc6b6b1bfbc33ac
lahotskyy <taras.lahotskyy@gmail.com>
2016-11-09 01:30:06 +0200

    fix common module

--- app/js/common/core.services.js
+++ app/js/common/core.services.js
@@ -2,14 +2,6 @@ var angular = require('angular');
 
 module.exports = angular.module("core.services",
   ["ngResource"])
-  .constant('server_statuses', {
-    UNPROCESSABLE_ENTITY: 422,
-    NOT_FOUND: 404
-  })
   .constant('appSettings',{
       SERVER_PATH: "http://localhost:3000/",
-      EMAIL_REGEX: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-  })
-  .constant('messages', {
-    NO_AUTH: "Invalid combination of email/password"
   });
