b9e9ff1819169a26e2ae55d4f340ffec22e5b920
kuriy-yevgeniy <kuriy.yevgeniy@gmail.com>
2016-11-09 16:32:54 +0200

    School service test

--- app/js/schools/services/schools.spec.js
+++ app/js/schools/services/schools.spec.js
@@ -1,19 +1,19 @@
-var StudentFactory = require('./schools.service.js');
+var SchoolFactory = require('./schools.service.js');
 
 describe('Schools factory ', function() {
-  var SchoolResource;
+  var School;
 
-  // Before each test load our app.schools.resource module
+  // Before each test load our app.schools. module
   beforeEach(angular.mock.module(SchoolFactory.name));
 
   // Before each test set our injected School factory to our local School variable
-  beforeEach(inject(function(_SchoolResource_) {
-    SchoolResource = _SchoolResource_;
+  beforeEach(inject(function(_School_) {
+    School = _School_;
   }));
 
   // A simple test to verify the School factory exists
   it('should exist', function() {
-    expect(SchoolResource).toBeDefined();
+    expect(School).toBeDefined();
   });
 
 });
