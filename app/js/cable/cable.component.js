71d19dd1cd4a3a0e9084ee2447bcc4993b816587
zoryana94 <zoryanavakh@gmail.com>
2016-11-09 19:45:45 +0200

    Add some content

--- app/js/cable/cable.component.js
+++ app/js/cable/cable.component.js
@@ -7,29 +7,20 @@ module.exports = angular.module('app.chat.component', [])
      templateUrl: './app/js/cable/cable.template.html',
      controller: ['$scope',
       function CableController($scope){
-        var cable = ActionCable.createConsumer('ws://localhost:3000/cable');
-
-        // cable.subscriptions.create('AppearanceChannel', {
-        //   // normal channel code goes here...
-        // });
+        var cable = ActionCable.createConsumer();
+        $scope.inputText = "";
+        $scope.myData = [];
         cable.subscriptions.create('ChatChannel', {
           // normal channel code goes here...
+          connected: function sendToMyChannel(message) {
+            // cable.send(message);
+          },
+          disconnected: function() {},
+          received: function(data) {
+
+          }
+
         });
       }]
 
-
-    //  ['$scope', 'ActionCableChannel', function SomeController($scope, ActionCableChannel){
-    //   $scope.inputText = "";
-    //   $scope.myData = [];
-    //
-    //   // connect to ActionCable
-    //   var consumer = new ActionCableChannel("MyChannel");
-    //   var callback = function(message){ $scope.myData.push(message); };
-    //   // consumer.subscribe(callback).then(function(){
-    //   //   $scope.sendToMyChannel = function(message){ consumer.send(message, 'send_a_message'); };
-    //   //   $scope.$on("$destroy", function(){
-    //   //     consumer.unsubscribe().then(function(){ $scope.sendToMyChannel = undefined; });
-    //   //   });
-    //   // });
-    // }]
   });
