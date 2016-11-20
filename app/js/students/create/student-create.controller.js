b330c47d6aab3a80c9aa1db84672218025658096
lahotskyy <taras.lahotskyy@gmail.com>
2016-11-20 19:16:48 +0200

    Implemented possibility to add Parent when create Student

--- app/js/students/create/student-create.controller.js
+++ app/js/students/create/student-create.controller.js
@@ -4,18 +4,18 @@ module.exports = angular
   .module('app.students.createController',[])
   .controller('StudentCreateController', StudentCreateController);
 
-  StudentCreateController.$inject = ['$scope', '$state', 'StudentResource', 'Group', 'ParentResource'];
-  function StudentCreateController ($scope, $state,  StudentResource, Group, ParentResource) {
-    $scope.student = new StudentResource();
-    Group.get(function(data){
-      $scope.groups = data.groups;
+StudentCreateController.$inject = ['$scope', '$state', 'StudentResource', 'Group', 'ParentResource'];
+function StudentCreateController ($scope, $state,  StudentResource, Group, ParentResource) {
+  $scope.student = new StudentResource();
+  Group.get(function(data){
+    $scope.groups = data.groups;
+  });
+  ParentResource.get(function(data){
+    $scope.parents = data.parents;
+  });
+  $scope.addStudent = function() {
+      $scope.student.$save(function() {
+      $state.go('students');
     });
-    ParentResource.get(function(data){
-      $scope.parents = data.parents;
-    });
-    $scope.addStudent = function() {
-        $scope.student.$save(function() {
-        $state.go('students');
-      });
-    }
-  }
+  };
+}
