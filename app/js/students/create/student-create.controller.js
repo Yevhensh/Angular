479b26effcdbaab9e1d2f652e6e03362114c9df1
lahotskyy <taras.lahotskyy@gmail.com>
2016-11-20 16:35:16 +0200

    Refactor Students component

--- app/js/students/create/student-create.controller.js
+++ app/js/students/create/student-create.controller.js
@@ -1,25 +1,18 @@
 'use strict';
 
 module.exports = angular
-  .module('app.studentCreate.component',[])
-  .component('studentCreate', {
-    templateUrl: '/app/js/students/create/student-create.template.html',
-    controller: StudentCreateController
-  });
+  .module('app.students.createController',[])
+  .controller('StudentCreateController', StudentCreateController);
 
   StudentCreateController.$inject = ['$scope', '$state', 'StudentResource', 'Group', 'ParentResource'];
-
   function StudentCreateController ($scope, $state,  StudentResource, Group, ParentResource) {
     $scope.student = new StudentResource();
-
     Group.get(function(data){
       $scope.groups = data.groups;
     });
-
     ParentResource.get(function(data){
       $scope.parents = data.parents;
     });
-
     $scope.addStudent = function() {
         $scope.student.$save(function() {
         $state.go('students');
