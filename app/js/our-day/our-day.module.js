016b05679f75e927c497332a84be805a80e2ed7f
zoryana94 <zoryanavakh@gmail.com>
2016-11-28 08:06:41 +0200

    Remove some components

--- app/js/our-day/our-day.module.js
+++ app/js/our-day/our-day.module.js
@@ -1,14 +1,10 @@
 var ourDayListComponent = require('./components/our-day-list/our-day-list.component.js'),
-    ourDayCreateComponent = require('./components/our-day-create/our-day-create.component.js'),
-    ourDayDetailComponent = require('./components/our-day-detail/our-day-detail.component.js'),
     ourDayEditComponent = require('./components/our-day-edit/our-day-edit.component.js'),
     ourDayService = require('./our-day.service.js'),
     ourDayRoute = require('./our-day.route.js');
 
 module.exports = angular.module("app.ourDay", [
   ourDayListComponent.name,
-  ourDayCreateComponent.name,
-  ourDayDetailComponent.name,
   ourDayEditComponent.name,
   ourDayService.name,
   ourDayRoute.name
